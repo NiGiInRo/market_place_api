83dfb77b22e0e6b3e6b15bc857c17b28b7e1a150
Nicolas <ninfante@e-learning.com.co>
2022-04-20 22:19:56 -0500

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   validates :email, uniqueness: true #que el email sea único
   validates_format_of :email, with: /@/ #que el email tenga un formato válido
   validates :password_digest, presence: true #que la contraseña siempre contenga algo
+
+  # • La contraseña debe estar presente en la creación.
+  # • La longitud de la contraseña debe ser menor o igual a 72 bytes.
+  # • La confirmación de la contraseña password_confirmation (si es enviado)
+  has_secure_password
 end
